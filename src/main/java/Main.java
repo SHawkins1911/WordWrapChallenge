71b9b400117a433552feb9276e7faade890237a5
stefunhawkins <hawkd215@gmail.com>
2019-10-15 11:37:10 -0400

    add documentation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //Create a Scanner to capture input string
         Scanner scanner = new Scanner(System.in);
 
+        //Request a sentence from the user
         System.out.println("Please enter a sentence");
+
+        //Print input string wrapped with indicated wrapLength
         System.out.print(WordUtils.wrap(scanner.nextLine(),13));
 
 
